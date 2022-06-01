e6ad9b16161abaff5603c2a89d68bf594dbd54b9
Rafaelitos17 <rafael.amoyan@mail.ru>
2022-06-02 01:22:07 +0300

    5 hw

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
+import {PATH} from "./Pages";
+import s from "./Header.module.css"
+
+type ActionType = {
+    [key: string]: boolean
+}
 
 function Header() {
+    const setAction = ({isActive}: ActionType) => isActive ? s.active : s.item
     return (
         <div>
-            // add NavLinks
-
+            <NavLink to={PATH.PRE_JUNIOR}
+                     className={navData => navData.isActive ? s.active : s.link}>Pre_Junior</NavLink>
+            <NavLink to={PATH.JUNIOR} className={navData => navData.isActive ? s.active : s.link}>Junior</NavLink>
+            <NavLink to={PATH.JUNIOR_PLUS}
+                     className={navData => navData.isActive ? s.active : s.link}>JuniorPlus</NavLink>
+            <div className={s.block}></div>
         </div>
     )
 }
